7eb25ee683c4872e538ad2b16ed72949fdb72dc0
tophtoph <cbschroed@gmail.com>
2016-12-18 21:35:02 -0500

    Commit removal of unnecessary header

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 # Chris Schroeder
 # R Programming, Coursera
 # Programming Assignment 2
